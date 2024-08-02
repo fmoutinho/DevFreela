8dcf86fc00db9f4de5154de4332bae5b4ed55f13
Fernando Moutinho <fernandomvmoutinho@outlook.com>
2024-08-02 19:48:16 -0300

    Add project files.

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,60 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(string search)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post(CreateProjectInputModel model)
+        {
+            return CreatedAtAction(nameof(GetById), new { id = 1 }, model);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, UpdateProjectInputModel model)
+        {
+            model.IdProject = id;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+
+        [HttpPut("{id}/Start")]
+        public IActionResult Start(int id)
+        {
+            return NoContent();
+        }
+
+        [HttpPut("{id}/Complete")]
+        public IActionResult Complete(int id)
+        {
+            return NoContent();
+        }
+
+        [HttpPost("{id}/comments")]
+        public IActionResult PostComment(CreateProjectCommentInputModel model)
+        {
+            return Ok();
+        }
+    }
+}
