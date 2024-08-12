d1ee97e9fd109f7680ce9e3f2e580d867ca76565
Fernando Moutinho <fernandomvmoutinho@outlook.com>
2024-08-12 20:22:45 -0300

    Use Options pattern to get maximum and minimum configs

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -1,7 +1,6 @@
 ﻿using DevFreela.API.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
-using System.Reflection.Metadata.Ecma335;
 
 namespace DevFreela.API.Controllers
 {
