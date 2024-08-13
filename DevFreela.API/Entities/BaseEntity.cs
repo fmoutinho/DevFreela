380843c6ca967cbc6945b3f4c52337015172941a
Fernando Moutinho <fernandomvmoutinho@outlook.com>
2024-08-13 00:41:07 -0300

    Create Domain

---
+++ DevFreela.API/Entities/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace DevFreela.API.Entities
+{
+    public abstract class BaseEntity
+    {
+        protected BaseEntity()
+        {
+            CreatedAt = DateTime.Now;
+            IsDeleted = false;
+
+        }
+        public int Id { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public bool IsDeleted { get; private set; }
+        public void SetAsDeleted() => IsDeleted = true;
+    }
+}
